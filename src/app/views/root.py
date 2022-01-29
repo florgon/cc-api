4c57ee68a165d105bd74265a64eb91b51fc3e18d
Kirill Zhosul <kirill_zhosul@vk.com>
2022-01-29 23:05:46 +0400

    Simple URL adding

--- src/app/views/root.py
+++ src/app/views/root.py
@@ -3,7 +3,10 @@
     URL shortener application root views.
 """
 
-from flask import Blueprint
+from flask import Blueprint, request
+
+from .. import db
+from ..models.url import Url
 
 bp_root = Blueprint("root", __name__)
 
@@ -13,6 +16,18 @@ def root_index():
     return "URL shortener application."
 
 
-@bp_root.route("/<url_index>", methods=["GET"])
-def root_url(url_index):
-    return str(url_index)
+@bp_root.route("/add", methods=["GET"])
+def root_add():
+    url_redirect = request.args.get("for")
+
+    if url_redirect:
+        url = Url(url_redirect)
+        db.session.add(url)
+        db.session.commit()
+        return f"Successfully added new URL with ID {url.id} that references (redirects) to URL {url_redirect}"
+    return "Failed to add new URL!"
+
+
+@bp_root.route("/<url_code>", methods=["GET"])
+def root_get(url_code):
+    return str(url_code)
